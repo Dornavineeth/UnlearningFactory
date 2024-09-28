8f1405dd24d16c31ec03111b9df678e5ce5b3de9
Dornavineeth <vineethdorna@gmail.com>
2024-09-28 06:30:46 +0000

    Fixed Tokenizer

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -13,6 +13,17 @@ def get_model(model_cfg: Dict):
         )
     return model
 
+def _add_or_replace_eos_token(tokenizer, eos_token: str) -> None:
+    is_added = tokenizer.eos_token_id is None
+    num_added_tokens = tokenizer.add_special_tokens({"eos_token": eos_token})
+
+    if is_added:
+        print("Add eos token: {}".format(tokenizer.eos_token))
+    else:
+        print("Replace eos token: {}".format(tokenizer.eos_token))
+
+    if num_added_tokens > 0:
+        print("New tokens have been added, make sure `resize_vocab` is True.")
 
 def get_tokenizer(tokenizer_cfg: Dict):
     tokenizer_name = tokenizer_cfg.get("name", None)
@@ -23,5 +34,10 @@ def get_tokenizer(tokenizer_cfg: Dict):
         raise NotImplementedError(
             f"{tokenizer_name} is not supported or Please use a valid tokenizer_name"
         )
-    # TODO: need to set the special/pad tokens correctly
+    if tokenizer.eos_token_id is None:
+        _add_or_replace_eos_token(tokenizer, eos_token="<|endoftext|>")
+    
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token = tokenizer.eos_token
+        print("Add pad token: {}".format(tokenizer.pad_token))
     return tokenizer
