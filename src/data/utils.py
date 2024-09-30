d9b27ffd3703bea4a15e528fa208de5c99522b73
molereddy <m.anmolreddy@gmail.com>
2024-09-30 06:22:05 +0000

    use dict instead of omegaconf dict

--- src/data/utils.py
+++ src/data/utils.py
@@ -7,16 +7,16 @@ import transformers
 IGNORE_INDEX = -100 # TODO put in constants
 
 def package_prompt_response(template_config, tokenizer, prompt, response, max_length):
-    if template_config.apply_chat_template:
+    if template_config["apply_chat_template"]:
         chat = [{"role": "user", "content": prompt}, 
                 {"role": "assistant", "content": response}]
-        if template_config.system_role:
-            chat = [{"role": "system", "content": "You are a helpful assistant."}]+chat
+        if template_config["system_role"]:
+            chat = [{"role": "system", "content": "You are a helpful assistant."}] + chat
         chat_ids = tokenizer.apply_chat_template(chat, tokenize=True, add_generation_prompt=False)
         wrapped_prompt = tokenizer.apply_chat_template(chat[:-1], tokenize=False, add_generation_prompt=True)
         prompt_ids = tokenizer.apply_chat_template(chat[:-1], tokenize=True, add_generation_prompt=True)
     else:
-        wrapped_prompt = template_config.user_start_tag + prompt + template_config.user_end_tag + template_config.asst_tag
+        wrapped_prompt = template_config["user_start_tag"] + prompt + template_config["user_end_tag"] + template_config["asst_tag"]
         chat_ids = tokenizer(wrapped_prompt + response, add_special_tokens=True, max_length=max_length, truncation=True)['input_ids']
         prompt_ids = tokenizer(wrapped_prompt, add_special_tokens=True, max_length=max_length, truncation=True)['input_ids']
     
