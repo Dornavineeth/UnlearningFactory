86cfc35b4a2958169bfe036264ab6f7d876c3cb8
molereddy <m.anmolreddy@gmail.com>
2024-09-30 13:39:32 +0000

    refactor collator code into separate file and call collator using get_collator

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,8 +1,5 @@
 import numpy as np
 import torch
-from dataclasses import dataclass
-from typing import Dict, Sequence
-import transformers
 
 IGNORE_INDEX = -100 # TODO put in constants
 
@@ -36,28 +33,3 @@ def add_dataset_index(dataset):
     dataset = dataset.add_column("index", indexing)
     return dataset
 
-@dataclass
-class DataCollatorForSupervisedDataset(object):
-    """Collate examples for supervised fine-tuning."""
-
-    tokenizer: transformers.PreTrainedTokenizer
-
-    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
-        input_ids, labels = tuple([instance[key] for instance in instances] for key in ("input_ids", "labels"))
-        input_ids = torch.nn.utils.rnn.pad_sequence(
-            input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id
-        )
-        labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=IGNORE_INDEX)
-        return dict(input_ids=input_ids, labels=labels, 
-                    attention_mask=input_ids.ne(self.tokenizer.pad_token_id))
-
-@dataclass
-class DataCollatorForSupervisedDatasetWithIndices(DataCollatorForSupervisedDataset):
-    """Collate examples for supervised fine-tuning and handle example indices."""
-
-    tokenizer: transformers.PreTrainedTokenizer
-
-    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
-        collated = super().__call__(instances)
-        collated['indices'] = torch.stack([example['indices'] for example in instances])
-        return collated
--- src/train.py
+++ src/train.py
@@ -1,11 +1,8 @@
 import hydra
 from omegaconf import DictConfig
-from data import get_dataset
+from data import get_dataset, get_collator
 from model import get_model
-from torch.utils.data import DataLoader
-from data.utils import DataCollatorForSupervisedDataset
 from trainer import load_trainer_args, load_trainer
-import sys
 
 @hydra.main(version_base=None, config_path="../configs", config_name="train.yaml")
 def main(cfg: DictConfig):
@@ -20,37 +17,31 @@ def main(cfg: DictConfig):
     # Load Dataset
     tokenizer_details = {'tokenizer': tokenizer, 'template_cfg': model_cfg.chat_templating}
     dataset = get_dataset("TOFU_QA", cfg.data, tokenizer_details)
+    collator = get_collator("DataCollatorForSupervisedDataset")
     
+    # Get Trainer
+    trainer_cfg = cfg.get("trainer", None)
+    trainer_name = trainer_cfg.get("name", None)
+    assert trainer_cfg is not None, ValueError("Please set trainer")
+    trainer_args = load_trainer_args(trainer_cfg)
+    trainer = load_trainer(
+        trainer_name=trainer_name,
+        trainer_args=trainer_args,
+        model=model,
+        train_dataset=dataset,
+        eval_dataset=dataset,
+        tokenizer=tokenizer,
+        data_collator=collator,
+    )
 
-    collator = DataCollatorForSupervisedDataset(tokenizer)
-    dataloader = DataLoader(dataset, collate_fn=collator, batch_size=3)
-    batch = next(iter(dataloader))
-    print(batch.keys())
+    trainer_args.do_train = True
+    trainer_args.do_eval = True
     
-    # # Get Trainer
-    # trainer_cfg = cfg.get("trainer", None)
-    # trainer_name = trainer_cfg.get("name", None)
-    # assert trainer_cfg is not None, ValueError("Please set trainer")
-    # trainer_args = load_trainer_args(trainer_cfg)
-    # trainer = load_trainer(
-    #     trainer_name=trainer_name,
-    #     trainer_args=trainer_args,
-    #     model=model,
-    #     train_dataset=dataset,
-    #     eval_dataset=dataset,
-    #     tokenizer=tokenizer,
-    #     # data_collator=None,
-    #     data_collator=DataCollatorForSupervisedDataset,
-    # )
+    if trainer_args.do_train:
+        trainer.train()
 
-    # trainer_args.do_train = True
-    # trainer_args.do_eval = True
-    
-    # if trainer_args.do_train:
-    #     trainer.train()
-
-    # if trainer_args.do_eval:
-    #     trainer.evaluate(metric_key_prefix="eval")
+    if trainer_args.do_eval:
+        trainer.evaluate(metric_key_prefix="eval")
 
 
 if __name__ == "__main__":
