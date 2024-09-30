e8a79628397bb1ff127c23db5bbe02fe998bf97f
molereddy <m.anmolreddy@gmail.com>
2024-09-30 04:08:03 +0000

    cleanup

--- configs/__init__.py
+++
@@ -1,7 +0,0 @@
-import hydra
-from omegaconf import DictConfig
-
-def get_config(config_type, name) -> DictConfig:
-    hydra.initialize(config_path=config_type)
-    cfg = hydra.compose(config_name=f"{name}.yaml")
-    return cfg
