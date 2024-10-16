478c825d4897a2af678e3680842d3b7172ebaf14
Vineeth <48151992+Dornavineeth@users.noreply.github.com>
2024-10-16 19:30:15 -0400

    Revert "Registry for metrics (#5)" (#7)
    This reverts commit d9467887011a8e40873b61fdc92daa5a666f0b88.

--- src/evals/base.py
+++ src/evals/base.py
@@ -1,4 +1,5 @@
 class Evaluator:
+    
     def __init__(self, eval_cfg, template_args, model, tokenizer, **kwargs):
         self.eval_cfg = eval_cfg
         self.template_args = template_args
--- src/evals/metrics/__init__.py
+++ src/evals/metrics/__init__.py
@@ -1,5 +1,6 @@
 from omegaconf import DictConfig
-from evals.metrics.memorization import (
+
+from .memorization import (
     qa_prob,
     qa_paraphrased_prob,
     qa_perturbed_prob,
@@ -7,44 +8,23 @@ from evals.metrics.memorization import (
     qa_paraphrased_rouge,
     qa_perturbed_rouge
 )
-    
-class unlearning_metric:
-    
-    def __init__(
-        self,
-        name: str,
-        data_cfg,
-        collator_cfg
-    ):
-        self.name = name
-        self.data_cfg = data_cfg
-        self.collator_cfg = collator_cfg
-    
-    
-    def __call__(self,  metric_fn: Callable[..., Any]) -> UnlearningMetric:
-        name = self.name or metric_fn.__name__
-        return UnlearningMetric(
-            name=name, 
-            data_cfg=self.data_cfg,
-            collator_cfg=self.collator_cfg,
-            metric_fn=metric_fn
-        )
-        # METRICS_REGISTRY[name] = UnlearningMetric(
-        #     name=name, 
-        #     data_cfg=self.data_cfg,
-        #     collator_cfg=self.collator_cfg,
-        #     metric_fn=metric_fn
-        # )
-        # return METRICS_REGISTRY[name]
 
-METRICS_REGISTRY: Dict[str, "UnlearningMetric"] = {}
 
 def _get_single_metric(metric_name, metric_cfg, **kwargs):
-    metric = METRICS_REGISTRY.get(metric_name)
-    if metric is None:
-        raise NotImplementedError(f"{metric_name} not implemented")
+    if metric_name == 'TOFU_QA_Prob':
+        return qa_prob
+    elif metric_name == 'TOFU_QA_P_Prob':
+        return qa_paraphrased_prob
+    elif metric_name == 'TOFU_QA_PT_Prob':
+        return qa_perturbed_prob
+    elif metric_name == 'TOFU_QA_ROUGE':
+        return qa_rouge
+    elif metric_name == "TOFU_QA_P_ROUGE":
+        return qa_paraphrased_rouge
+    elif metric_name == "TOFU_QA_PT_ROUGE":
+        return qa_perturbed_rouge
     else:
-        return metric
+        raise NotImplementedError(f"{metric_name} not implemented")
 
 def get_metrics(metric_cfgs: DictConfig, **kwargs):
     metrics = {}
