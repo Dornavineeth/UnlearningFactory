cb65205a4343364dc8a3a30a120050bbb2db0c6d
molereddy <m.anmolreddy@gmail.com>
2024-11-15 19:57:27 +0000

    refactor batch evals code fully, using common code for rouge and probability

--- src/evals/metrics/memorization.py
+++ src/evals/metrics/memorization.py
@@ -8,6 +8,44 @@ from collections import defaultdict
 
 from evals.metrics.base import unlearning_metric
 
+def dict_transpose(evals):
+    # multiple answers indexed by intra_item_idx, then item_idx
+    all_iidxs = list(evals.keys())
+    all_idxs = list(evals[all_iidxs[0]].keys())
+    all_stat_names = list(evals[all_iidxs[0]][all_idxs[0]].keys())
+    # invert the dict, put outermost iidx deepest inside
+    evals = {idx: {stat: [evals[iidx][idx][stat] 
+                            for iidx in all_iidxs]
+                    for stat in all_stat_names}
+                for idx in all_idxs}
+    return evals
+
+def run_batchwise_evals(model, dataloader, batch_eval_fn, batch_eval_fn_args, eval_msg):
+    evals = defaultdict(dict)
+    for batch in tqdm(dataloader, desc=eval_msg, total=len(dataloader)):
+        # if data arrives in normal format we convert the batch to multiple answer-style
+        # like in tofu_perturbed by adding a fake intra_item_index
+        if "input_ids" in batch:
+            batch = {"0": batch}
+        # Assume batch like {"0": {"input_ids": [[]]..., "index": [453, 454..]},
+        #                    "1": {"input_ids": [[]]..., "index": [453, 454..]}..}
+        assert isinstance(next(iter(batch.values())), dict) and "input_ids" in next(iter(batch.values()))
+        for intra_item_idx, mini_batch in batch.items():
+            data_indices = mini_batch.pop("index").cpu().numpy().tolist() # data item indices
+            batch_evals = batch_eval_fn(model=model, batch=mini_batch, **batch_eval_fn_args)
+            indexwise_batch_evals = dict(zip(data_indices, batch_evals))
+            assert not (evals[intra_item_idx].keys() & indexwise_batch_evals.keys()), \
+                "Data indices repeated while iterating dataloader"
+            evals[intra_item_idx] |= indexwise_batch_evals
+    # evals looks like {iidx0: {idx453: {prob: 0.1, loss: 1}},
+    #                   iidx1: {idx453: {prob: 0.2, loss: 2}}}
+    if len(evals) == 1: # normal single answer dataset, no need for list
+        return next(iter(evals.values()))
+    # for each index return a dict with all intra_item_idx values in list
+    # now looks like {idx453: {prob: [0.1, 0.2], loss: [1, 2]}}
+    evals = dict_transpose(evals)
+    return evals
+    
 
 def evaluate_probability_batch(model, batch):
     batch = {k: v.to(model.device) for k, v in batch.items()}
@@ -18,68 +56,42 @@ def evaluate_probability_batch(model, batch):
     shifted_labels = labels[..., 1:].contiguous()
     logits = logits[..., :-1, :].contiguous()
     loss_function = nn.CrossEntropyLoss(ignore_index=-100, reduction="none")
-    # get the sum loss for each sequence in a batch
-    loss = loss_function(logits.transpose(-1, -2), shifted_labels).sum(dim=-1)
+    # agg loss across tokens
+    losses = loss_function(logits.transpose(-1, -2), shifted_labels).sum(dim=-1)
     num_token_gt = (batch["labels"] != -100).sum(-1)
-    avg_loss = loss / num_token_gt
-    normalized_probs = torch.exp(-avg_loss)
-    return {'prob': normalized_probs.cpu().numpy().tolist(), 'avg_loss': avg_loss.cpu().numpy().tolist()}
+    avg_losses = losses / num_token_gt
+    normalized_probs = torch.exp(-avg_losses)
+    
+    avg_losses = avg_losses.cpu().numpy().tolist()
+    normalized_probs = normalized_probs.cpu().numpy().tolist()
+    return [{'prob': prob, 'avg_loss': avg_loss} 
+            for prob, avg_loss in zip(normalized_probs, avg_losses)]
 
 
 def evaluate_probability(model, dataloader):
-    evals = defaultdict(dict)
-    for batch in tqdm(dataloader, desc="Calculating loss", total=len(dataloader)):
-        # if data arrives in normal format we convert the batch to multiple answer-style
-        # as in tofu_perturbed by adding a fake answer index
-        if "input_ids" in batch:
-            batch = {0: batch}
-        # Assume batch like {"0": {"input_ids": [[]]..., "index": [453, 454..]}, 
-        #                   "1": {.., "index":  [453, 454..]}..}
-        assert isinstance(next(iter(batch.values())), dict) and "input_ids" in next(iter(batch.values()))
-        for intra_item_idx, mini_batch in batch.items():
-            data_indices = mini_batch.pop("index").cpu().numpy().tolist() # data item indices
-            batch_evals = evaluate_probability_batch(model, mini_batch) # batch_evals maps each attr to a list of length bsz
-            transpose_batch_evals = [  # a list of len bsz which maps for each attr to an indiv value
-                dict(zip(batch_evals.keys(), values)) 
-                for values in zip(*batch_evals.values())
-            ]
-            indexwise_batch_evals = dict(zip(data_indices, transpose_batch_evals)) # map data indices to attr maps
-            assert not (evals[intra_item_idx].keys() & indexwise_batch_evals.keys()), "Data indices repeated while iterating dataloader"
-            evals[intra_item_idx] |= indexwise_batch_evals # append to existing collection for index
-    
-    if len(evals) == 1: # normal single answer dataset
-        return evals[0]
-    else:
-        all_iidxs = list(evals.keys())
-        all_idxs = list(evals[all_iidxs[0]].keys())
-        all_stats = list(evals[all_iidxs[0]][all_idxs[0]].keys())
-        # invert the dict, put outermost key to deepest
-        evals = {idx: {stat: [evals[iidx][idx][stat] 
-                              for iidx in all_iidxs]
-                       for stat in all_stats}
-                 for idx in all_idxs}
-        return evals
-    
-
-
-def eval_rouge_recall(gen_outputs, ground_truths):
-    scorer = rouge_scorer.RougeScorer(["rouge1", "rougeL"], use_stemmer=True)
-    rouge1_recall = []
-    rougeL_recall = []
-    for gen, gt in zip(gen_outputs, ground_truths):
-        rouge_scores = scorer.score(gt, gen)
-        rouge1_recall.append(rouge_scores["rouge1"].recall)
-        rougeL_recall.append(rouge_scores["rougeL"].recall)
-    return {"rouge1_recall": rouge1_recall, "rougeL_recall": rougeL_recall}
+    fun_args = {}
+    return run_batchwise_evals(model, dataloader, 
+                               evaluate_probability_batch, fun_args, 
+                               "Calculating loss")
 
 
 def eval_text_similarity_batch(model, tokenizer, batch, generation_args):
+    
+    def eval_rouge_recall_batch(gen_outputs, ground_truths):
+        scorer = rouge_scorer.RougeScorer(["rouge1", "rougeL"], use_stemmer=True)
+        evals = []
+        for gen, gt in zip(gen_outputs, ground_truths):
+            rouge_scores = scorer.score(gt, gen)
+            evals.append([{'rouge1_recall': rouge_scores["rouge1"].recall,
+                        'rougeL_recall': rouge_scores["rougeL"].recall}])
+        return evals
+    
     batch = {k: v.to(model.device) for k, v in batch.items()}
     input_ids = batch["input_ids"]
     labels = batch["labels"]
-    input_text = tokenizer.batch_decode(input_ids, skip_special_tokens=True)
+    input_texts = tokenizer.batch_decode(input_ids, skip_special_tokens=True)
     tokens = [label[label != -100] for label in labels]
-    ground_truth = tokenizer.batch_decode(tokens, skip_special_tokens=True)
+    ground_truths = tokenizer.batch_decode(tokens, skip_special_tokens=True)
     attention_mask = batch["attention_mask"]
 
     output = model.generate(
@@ -88,43 +100,20 @@ def eval_text_similarity_batch(model, tokenizer, batch, generation_args):
         **generation_args,
         pad_token_id=tokenizer.eos_token_id,
     )
-    gen_text = tokenizer.batch_decode(
+    gen_texts = tokenizer.batch_decode(
         output[:, input_ids.shape[-1] :], skip_special_tokens=True
     )
-    scores = eval_rouge_recall(gen_text, ground_truth)
-    scores.update(
-        {"input": input_text, "ground_truth": ground_truth, "generation": gen_text}
-    )
-    # return list of dictionaries
-    scores = [dict(zip(scores.keys(), values)) for values in zip(*scores.values())]
+    scores = eval_rouge_recall_batch(gen_texts, ground_truths)
+    scores = [{**rouge_evals, "input": input_text, "ground_truth": ground_truth, "generation": gen_text}
+              for rouge_evals, input_text, ground_truth, gen_text in zip(scores, input_texts, ground_truths, gen_texts)]
     return scores
 
 
 def eval_text_similarity(model, tokenizer, dataloader, generation_args):
-    index_to_scores = {}
-    for batch in tqdm(
-        dataloader, desc="Calculating Text Similarity", total=len(dataloader)
-    ):
-        if "input_ids" in batch:
-            batch = {0: batch}
-        assert isinstance(next(iter(batch.values())), dict) and "input_ids" in next(
-            iter(batch.values())
-        )
-        for _, mini_batch in batch.items():
-            index = mini_batch.pop("index").numpy().tolist()
-            scores = eval_text_similarity_batch(
-                model, tokenizer, mini_batch, generation_args
-            )
-            assert len(index) == len(scores)
-            for idx, score in zip(index, scores):
-                if idx in index_to_scores:
-                    if isinstance(index_to_scores[idx], list):
-                        index_to_scores[idx].append(score)
-                    else:
-                        index_to_scores[idx] = [index_to_scores[idx]] + [score]
-                else:
-                    index_to_scores[idx] = score
-    return index_to_scores
+    fun_args = {'tokenizer': tokenizer, 'generation_args': generation_args}
+    return run_batchwise_evals(model, dataloader, 
+                               eval_text_similarity_batch, fun_args, 
+                               "Calculating text similarity")
 
 
 @unlearning_metric(name="probability")
