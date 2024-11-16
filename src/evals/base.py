d727213662c50151006aaa837c5193d3b4c6b895
molereddy <m.anmolreddy@gmail.com>
2024-11-16 16:22:48 +0000

    Revert "revert sync"
    This reverts commit 2c94cff7c852a9e0dac15b391ac0868148455009.

--- src/evals/base.py
+++ src/evals/base.py
@@ -4,57 +4,76 @@ from evals.metrics import get_metrics
 
 
 class Evaluator:
-    def __init__(self, name, eval_cfg, template_args, model, tokenizer, **kwargs):
+    def __init__(self, name, eval_cfg, **kwargs):
         self.name = name
         self.eval_cfg = eval_cfg
-        self.template_args = template_args
-        self.model = model
-        self.tokenizer = tokenizer
-        self.prepare_model()
-        self.load_metrics()
-        self.load_logs_from_file()
-
-    def load_logs_from_file(self):
-        self.logs = {}
-        self.logs_filename = os.path.join(
-            self.eval_cfg.output_dir, f"{self.name}_EVAL.json"
-        )
-        if os.path.exists(self.logs_filename):
-            print(f"Loading existing evaluations from {self.logs_filename}")
-            with open(self.logs_filename, "r") as f:
-                self.logs = json.load(f)
-
-    def save_logs(self):
-        os.makedirs(self.eval_cfg.output_dir, exist_ok=True)
-        with open(self.logs_filename, "w") as f:
-            json.dump(self.logs, f, indent=4)
-
-    def prepare_model(self):
-        self.device = self.eval_cfg.device
-        self.model.to(self.device)
-        self.model.eval()
-
-    def load_metrics(self):
         self.metrics_cfg = self.eval_cfg.metrics
-        self.metrics = get_metrics(self.metrics_cfg)
+        self.metrics = self.load_metrics(self.metrics_cfg)
 
-    def evaluate(self, overwrite=False, **kwargs):
+    def get_logs_file_path(self, output_dir):
+        """Returns the path to json file to store results"""
+        logs_filename = os.path.join(output_dir, f"{self.name}_EVAL.json")
+        return logs_filename
+
+    def load_logs_from_file(self, file):
+        """Returns the cache of existing results"""
+        logs = {}
+        if os.path.exists(file):
+            print(f"Loading existing evaluations from {file}")
+            with open(file, "r") as f:
+                logs = json.load(f)
+        return logs
+
+    def save_logs(self, logs, file):
+        """Save the logs in a json file"""
+        logs = dict(sorted(logs.items()))
+        os.makedirs(os.path.dirname(file), exist_ok=True)
+        with open(file, "w") as f:
+            json.dump(logs, f, indent=4)
+
+    def prepare_model(self, model):
+        """Prepare model for evaluation"""
+        self.device = self.eval_cfg.device
+        model.to(self.device)
+        model.eval()
+        return model
+
+    def load_metrics(self, metrics_cfg):
+        """Load metrics for evaluation"""
+        metrics = get_metrics(metrics_cfg)
+        return metrics
+
+    def evaluate(self, model, output_dir=None, overwrite=None, **kwargs):
+        # set flag to overwrite metrics
+        overwrite = self.eval_cfg.overwrite if overwrite is None else overwrite
+        
+        # Prepare model for evaluation
+        model = self.prepare_model(model)
+
+        # Set output_dir and file to store results
+        output_dir = output_dir if output_dir else self.eval_cfg.output_dir
+        logs_file_path = self.get_logs_file_path(output_dir)
+
+        # Load exisiting results from file if any.
+        logs = self.load_logs_from_file(logs_file_path)
+
         print(f"***** Running Evaluation {self.name} *****")
         for metric_name, metric_fn in self.metrics.items():
-            if not overwrite and metric_name in self.logs:
+            if not overwrite and metric_name in logs:
                 print(f"Skipping {metric_name}, already evaluated.")
                 continue
-            _ = self.logs.pop(
-                metric_name, None
-            )  # overwriting existing evals if present
-            kwargs = {"tokenizer": self.tokenizer, "template_args": self.template_args}
+            _ = logs.pop(metric_name, None)  # overwriting existing evals if present
+            kwargs = {
+                "tokenizer": kwargs.get("tokenizer", None),
+                "template_args": kwargs.get("template_args", None),
+            }
             metrics_args = self.eval_cfg.metrics[metric_name]
             _ = metric_fn(
-                self.model,
+                model,
                 metric_name=metric_name,
-                cache=self.logs,
+                cache=logs,
                 **kwargs,
                 **metrics_args,
             )
-            self.save_logs()
-        return self.logs
+            self.save_logs(logs, logs_file_path)
+        return logs
