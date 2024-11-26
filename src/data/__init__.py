aab9dc38aadc38d4b867299f0c59024bc5ed9110
Dornavineeth <vineethdorna@gmail.com>
2024-11-26 20:14:33 +0000

    rename fix to anchor

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -53,14 +53,14 @@ def get_datasets(dataset_cfgs: Union[Dict, DictConfig], **kwargs):
 def get_data(data_cfg: DictConfig, mode="train", **kwargs):
     data = {}
     data_cfg = dict(data_cfg)
-    fix = data_cfg.pop("fix", "forget")
+    anchor = data_cfg.pop("anchor", "forget")
     for split, dataset_cfgs in data_cfg.items():
         data[split] = get_datasets(dataset_cfgs, **kwargs)
     if mode == "train":
         return data
     elif mode == "unlearn":
         unlearn_splits = {k: v for k, v in data.items() if k not in ("eval", "test")}
-        unlearn_dataset = ForgetRetainDataset(**unlearn_splits, fix=fix)
+        unlearn_dataset = ForgetRetainDataset(**unlearn_splits, anchor=anchor)
         data["train"] = unlearn_dataset
         for split in unlearn_splits:
             data.pop(split)
--- src/data/unlearn.py
+++ src/data/unlearn.py
@@ -4,40 +4,40 @@ from torch.utils.data import Dataset
 
 class ForgetRetainDataset(Dataset):
     # https://github.com/OPTML-Group/SOUL/blob/main/src/dataset/Base.py
-    def __init__(self, forget, retain, fix="forget"):
+    def __init__(self, forget, retain, anchor="forget"):
         """Wraps the forget retain dataset into unlearning dataset.
 
         Args:
             forget (Dataset): Forget Dataset
             retain (Dataset): Retain Dataset
-            fix (str, optional): Specifies which dataset to fix while sampling from the other. Defaults to 'forget'.
+            anchor (str, optional): Specifies which dataset to anchor while sampling from the other. Defaults to 'forget'.
         """
         self.forget = forget
         self.retain = retain
-        self.fix = fix
+        self.anchor = anchor
 
     def __len__(self):
-        if self.fix == "forget":
+        if self.anchor == "forget":
             assert self.forget is not None, ValueError(
-                "forget dataset can't be None when fix=forget"
+                "forget dataset can't be None when anchor=forget"
             )
             return len(self.forget)
-        elif self.fix == "retain":
+        elif self.anchor == "retain":
             assert self.forget is not None, ValueError(
-                "retain dataset can't be None when fix=retain"
+                "retain dataset can't be None when anchor=retain"
             )
             return len(self.retain)
         else:
-            raise NotImplementedError(f"{self.fix} can be only forget or retain")
+            raise NotImplementedError(f"{self.anchor} can be only forget or retain")
 
     def __getitem__(self, idx):
         item = {}
-        if self.fix == "forget":
+        if self.anchor == "forget":
             item["forget"] = self.forget[idx]
             if self.retain:
                 retain_idx = torch.randint(0, len(self.retain), (1,)).item()
                 item["retain"] = self.retain[retain_idx]
-        elif self.fix == "retain":
+        elif self.anchor == "retain":
             item["retain"] = self.retain[idx]
             if self.forget:
                 forget_idx = torch.randint(0, len(self.forget), (1,)).item()
