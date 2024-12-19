14c8aa1feb80f28cbf47504c85e7af3e081f8bb4
Dornavineeth <vineethdorna@gmail.com>
2024-12-19 20:02:27 +0000

    Support privacy metrics using retain model.

--- src/evals/metrics/base.py
+++ src/evals/metrics/base.py
@@ -1,3 +1,5 @@
+import os
+import json
 import logging
 from typing import Callable, Any, Dict
 from data import get_datasets, get_collators
@@ -45,22 +47,35 @@ class UnlearningMetric:
         results = self._metric_fn(model, **kwargs)
         return results
 
+    def load_logs_from_file(self, file):
+        """Load a logs file, assumes json"""
+        logs = {}
+        if os.path.exists(file):
+            logger.info(f"Loading evaluations from {file}")
+            with open(file, "r") as f:
+                logs = json.load(f)
+        return logs
+
+
     def prepare_kwargs_evaluate_metric(self, model, metric_name, cache={}, **kwargs):
         """Prepare the kwargs required to call the metric_fn defined by user.
         - Loads datasets, collators, results for pre_compute metrics
         Returns:
             Dict: Updated kwargs with datasets, collators, pre_compute results loaded
         """
+        # Load datasets
         dataset_cfgs = kwargs.pop("datasets", None)
         if dataset_cfgs is not None:
             data = self.get_datasets(dataset_cfgs=dataset_cfgs, **kwargs)
             kwargs.update({"data": data})
-
+        
+        # Load collators
         collator_cfgs = kwargs.pop("collators", None)
         if collator_cfgs is not None:
             collators = self.get_collators(collator_cfgs=collator_cfgs, **kwargs)
             kwargs.update({"collators": collators})
 
+        # Evaluate precompute and load results
         pre_compute_cfgs = kwargs.pop("pre_compute", {})
         pre_metric_results = {}
         for pre_metric_name, pre_metric_cfg in pre_compute_cfgs.items():
@@ -82,7 +97,29 @@ class UnlearningMetric:
                     model, pre_metric_name, cache=cache, **pre_metric_kwargs
                 )
             pre_metric_results.update({access_name: _results})
-        kwargs.update({"pre_compute": pre_metric_results})
+        if pre_metric_results:
+            kwargs.update({"pre_compute": pre_metric_results})
+        
+        # Load reference logs
+        reference_logs_cfgs = kwargs.pop("reference_logs", {})
+        reference_logs = {}
+        for reference_log_name, reference_log_cfg in reference_logs_cfgs.items():
+            path = reference_log_cfg.get("path", None)
+            if path is None:
+                continue
+            include_cfgs = reference_log_cfg.get("include", None) 
+            assert path is not None, ValueError("path not specified for {reference_log_name} in {metric_name}")
+            _logs = self.load_logs_from_file(path)
+            reference_logs[reference_log_name] = {}
+            for key, include_cfg in include_cfgs.items():
+                access_name = include_cfg.get("access_key", key)
+                _results = _logs.get(key, None)
+                reference_logs[reference_log_name][access_name] = _results
+                if _results is None:
+                    logger.warning(f"{key} not present in the {path}, setting it to None!")
+        if reference_logs:
+            kwargs.update({"reference_logs": reference_logs})
+
         return kwargs
 
     def evaluate(self, model, metric_name, cache, **kwargs):
